55a92fdadd714dd791d8f28a85b61af549af1ab4
Samuel Kipnis <sam.kipnis@gmail.com>
2025-02-25 11:01:26 -0500

    Update readSerial.js

--- triviagame/readSerial/readSerial.js
+++ triviagame/readSerial/readSerial.js
@@ -1,6 +1,27 @@
 import { SerialPort } from 'serialport';
 
-SerialPort.list().then(
-	(ports) => ports.forEach(console.log),
-	(err) => console.error(err)
-);
+async function readSerial() {
+	let serialPort;
+	await SerialPort.list().then(
+		(ports) =>
+			ports.forEach((port) => {
+				if (port.manufacturer === 'Silicon Labs') serialPort = port;
+			}),
+		(err) => console.error(err)
+	);
+
+	const port = new SerialPort({
+		path: serialPort.path,
+		baudRate: 115200,
+	});
+
+	try {
+		port.on('readable', () => {
+			console.log('Data:', port.read());
+		});
+	} catch (error) {
+		console.log('failed to connect');
+	}
+}
+
+readSerial();
